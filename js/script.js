dcb81af39b8e3cf00b7c68e93e3d1c5e33d64472
mary-wan <mary.njenga@student.moringaschool.com>
2021-09-17 15:12:34 +0300

    add background

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+let map;
 
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat:  -1.232667,
+            lng: 36.878592
+        },
+        zoom: 18,
+    });
+}
