d2c967522d2e9065dcad62200e21017bd65b5288
mary-wan <mary.njenga@student.moringaschool.com>
2021-09-18 21:37:43 +0300

    add home delievry

--- js/script.js
+++ js/script.js
@@ -10,19 +10,21 @@ $(document).ready(function () {
             this.total = total;
 
         }
-        // pizza.prototype.checkoutPrice = function(){
-
-        // }
         var inputtedType = $("#pizzaType").find(":selected").text();
         var inputtedSize = $("#pizzaSize").find(":selected").val();
         var inputtedCrust = $("#pizzaCrust").find(":selected").val();
         var inputtedTopping = $('input[name="crusts"]:checked').val();
+        var homeCheckoutTotal =0
+        var checkoutTotal =0;
 
         if (inputtedSize == ("select")) {
+            alert("Please fill in all details");
             $(".display").text("Please select pizza Size");
         } else if (inputtedCrust == ("select")) {
+            alert("Please fill in all details");
             $(".display").text("Please select pizza Crust");
         } else if (inputtedTopping == (undefined)) {
+            alert("Please fill in all details");
             $(".display").text("Please select pizza Topping");
         } else {
             $(".display").hide();
@@ -58,31 +60,55 @@ $(document).ready(function () {
             var pizzaOrder = new pizza(inputtedType, inputtedSize, inputtedCrust, inputtedTopping, totalPrice);
 
             $(".table").append("<tr><td>" + pizzaOrder.type + "</td><td>" + pizzaOrder.size + "</td><td>" +
-                pizzaOrder.crust + "</td><td>" + pizzaOrder.topping + "</td><td>" + pizzaOrder.total + "</td></tr>");
+                pizzaOrder.crust + "</td><td>" + pizzaOrder.topping + "</td><td>" + pizzaOrder.total + "</td></tr>"
+            );
 
 
         }
         $("#checkout").click(function () {
             $("#add").hide();
             $("#checkout").hide();
-            $("#checkoutMessage").show();
+            $(".checkoutMessage").show()
+
             var tbl = document.querySelector("table");
             var tableRow = tbl.getElementsByTagName("TR");
-            var checkoutTotal = 0;
             for (let i = 1; i < tableRow.length; i++) {
                 var col = tableRow[i].getElementsByTagName("TD");
                 checkoutTotal += parseFloat(col[4].innerHTML);
             }
-            $(".checkoutMessage").show().text("The total Price is " + checkoutTotal + ". Your pizza  will be ready for pickup in 30 minutes. We look forward to serving you again. ");
-            $("#homeDelivery").css("margin-top", "70px");
+            $("#checkoutMessage").text("Your bill  is " + checkoutTotal + ". Your pizza  will be ready for pickup in 30 minutes. We look forward to serving you again. ");
+            $("#homeDelivery").css("margin-top", "40px");
             $("#homeDelivery").css("margin-left", "20%");
 
         });
+        
+        $("#homeDelivery").click(function(){
+            $("#homeDelivery").hide();
+            $("#checkoutMessage").hide();
+            $(".address").show();
+            $(".table").hide();
+            
+        })
+        $("#homeCheckout").click(function(e){
+            e.preventDefault();
 
+            homeCheckoutTotal = checkoutTotal + 150;
+            var name = $("input#name").val();
+            var phone = $("input#phonenum").val();
+            var location = $("input#location").val()
 
+            if((name == "" ||name == null)||(phone == "" ||phone == null) || (location == "" ||location == null)){
+                alert("Please fill in all details")
+            }else{
+                $(".address").hide(); 
+                $(".table").show();
+                $("#checkoutMessage").show().text("Dear " +name + ", Your bill plus the delivery price is " + homeCheckoutTotal + ".Your order will be delivered to " +location+ " in 1 hour. Kindly provide the payment on delivery");
+            }
+
+            
+            
+
+        });
     });
-
-
-
 
 });
